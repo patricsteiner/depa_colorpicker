ff7503a57ed049a829e3273b4e79abe289a992f4
Patric Steiner <Patric Steiner>
2016-09-21 22:01:13 +0200

    bind rectangle to sliders, no colors yet though

--- src/depa_colorpicker/ColorPicker.java
+++ src/depa_colorpicker/ColorPicker.java
@@ -1,18 +1,16 @@
 package depa_colorpicker;
 
+
+
 import javafx.beans.binding.Bindings;
-import javafx.beans.property.IntegerProperty;
-import javafx.beans.property.SimpleIntegerProperty;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
-import javafx.geometry.Orientation;
 import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.Background;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.util.converter.NumberStringConverter;
 
 public class ColorPicker extends BorderPane {
@@ -20,6 +18,7 @@ public class ColorPicker extends BorderPane {
 	Slider[] sliders = new Slider[] { new Slider(), new Slider(), new Slider() };
 	TextField[] textFields = new TextField[] { new TextField(), new TextField(), new TextField() };
 	Label[] labels = new Label[] { new Label(), new Label(), new Label() };
+	Rectangle rect = new Rectangle(100,100);
 	
 	public ColorPicker() {
 		setLeft(new VBox(sliders));
@@ -31,8 +30,11 @@ public class ColorPicker extends BorderPane {
 			Bindings.bindBidirectional(textFields[i].textProperty(), sliders[i].valueProperty(), new NumberStringConverter());
 			labels[i].textProperty().bind(sliders[i].valueProperty().asString());
 			labels[i].setDisable(true);
+			sliders[i].valueProperty().addListener((prop, o, n) -> rect.setFill(Color.rgb(n.intValue(), n.intValue(), n.intValue())));
 			//sliders[i].valueProperty().addListener((prop, o, n) -> labels[i].setText(n.toString()));
 		}
+		
+		setBottom(rect);
 		
 	}
 }
